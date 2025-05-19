56092e7e2bdfe881fc0b0a20955c0979d994cdd7
maurilio.medeiros <Mebnsm@10725>
2025-05-19 16:38:42 -0300

    Refactor form layout and styling for ModeloEmpilhadeira; enhance input fields and labels

--- src/Form/ModeloEmpilhadeiraType.php
+++ src/Form/ModeloEmpilhadeiraType.php
@@ -3,7 +3,11 @@
 namespace App\Form;
 
 use App\Entity\ModeloEmpilhadeira;
+use Doctrine\DBAL\Types\FloatType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,32 @@ class ModeloEmpilhadeiraType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('fabricante')
-            ->add('modelo')
-            ->add('cargaTotal')
-            ->add('caracteristicas')
-            ->add('aplicacoes')
+            ->add('fabricante', TextType::class, [
+                'required' => true,
+                'label' => 'Fabricante:*',
+            ])
+            ->add('modelo', TextType::class, [
+                'required' => true,
+                'label' => 'Modelo:*',
+            ])
+            ->add('cargaTotal', NumberType::class, [
+                'required' => true,
+                'label' => 'Carga Total:*',
+            ])
+            ->add('caracteristicas', TextareaType::class, [
+                'required' => true,
+                'label' => 'Características:*',
+                'attr' => [
+                    'rows' => 3,
+                ],
+            ])
+            ->add('aplicacoes', TextareaType::class, [
+                'required' => true,
+                'label' => 'Aplicações:*',
+                'attr' => [
+                    'rows' => 3,
+                ],
+            ])
         ;
     }
 
