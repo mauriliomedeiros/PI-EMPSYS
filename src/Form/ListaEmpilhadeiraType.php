bc76e77658c14abcd7dbd9b6ec3a2213d2d02164
maurilioebnsm <M@uGit2024!>
2025-05-28 23:52:56 -0300

    formType editado

--- src/Form/ListaEmpilhadeiraType.php
+++ src/Form/ListaEmpilhadeiraType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\ListaEmpilhadeira;
+use App\Entity\Local;
+use App\Entity\ModeloEmpilhadeira;
+use App\Entity\Cliente;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +16,24 @@ class ListaEmpilhadeiraType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('modelo')
-            ->add('cliente')
-            ->add('local')
+            ->add('modelo', EntityType::class, [
+                'class' => ModeloEmpilhadeira::class,
+                'choice_label' => 'modelo',
+                'label' => 'Modelo:*',
+                'placeholder' => '---Selecione---',
+            ])
+            ->add('cliente', EntityType::class, [
+                'class' => Cliente::class,
+                'choice_label' => 'razao_social',
+                'label' => 'Cliente:*',
+                'placeholder' => '---Selecione---',
+            ])
+            ->add('local', EntityType::class, [
+                'class' => Local::class,
+                'choice_label' => 'nome',
+                'label' => 'Local:*',
+                'placeholder' => '---Selecione---',
+            ])
         ;
     }
 
