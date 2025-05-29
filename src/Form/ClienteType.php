82bf7d47d9da1448b774fc8ec46f32e75a92f442
maurilioebnsm <M@uGit2024!>
2025-05-28 22:52:14 -0300

    Aplicando estética no Cliente

--- src/Form/ClienteType.php
+++ src/Form/ClienteType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Cliente;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +13,19 @@ class ClienteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('razao_social')
-            ->add('nome_fantasia')
-            ->add('cnpj')
-            ->add('ativo')
-        ;
+            ->add('razao_social', TextType::class, [
+                'required' => true,
+                'label' => 'Razão Social:*',
+            ])
+            ->add('nome_fantasia', TextType::class, [
+                'required' => true,
+                'label' => 'Nome Fantasia:*',
+            ])
+            ->add('cnpj', TextType::class, [
+                'required' => true,
+                'label' => 'CNPJ:*',
+            ])
+            ->add('ativo');
     }
 
     public function configureOptions(OptionsResolver $resolver): void
